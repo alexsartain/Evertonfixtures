cf33c189b7e66693384be61e72b136cb0ca3d3eb
alexsartain <11asartain@gmail.com>
2017-06-15 15:18:15 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+$('#h').on('click',function(){
+$('#a').hide();
+});
+
+});
+$(document).ready(main);
+var aug=$('.august');
+var sep=$('.September');
+var oct=$('.October');
+var nov=$('.November');
+var dec=$('.December');
+var jan=$('.January');
+var feb=$('.February');
+var mar=$('.March');
+var apr=$('.April');
+var may=$('.May');
+
